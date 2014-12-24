19e29443db86994e90c4bdfdda29901e7ac09516
ggn <ggn.dbug@gmail.com>
2014-12-25 01:01:48 +0200

    Cleaned up dirs, tagged release v0.1.8.

--- buildprint.bat
+++
@@ -1,5 +0,0 @@
-m68k-atari-mint-c++.exe print.c -Iinclude -S -O2
-m68k-atari-mint-c++.exe print.c -Iinclude -c -O2
-rln -z -rq -o RBASIC.ABS -a 4000 x x build\BASIC.O RAPTOR\RAPTOR.O U235SE.021\DSP.OBJ obj\libm.a obj\libc.a obj\libgcc.a print.o
-bin\virtualjaguar RBASIC.ABS
-
--- print.bas
+++
@@ -1,3 +0,0 @@
-dim a,b,c,d$
-print "bellooooooooooo",a,b,c,d$
-
