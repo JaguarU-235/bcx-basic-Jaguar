5666f2064a6de98dd63537a1875226bf396479e9
ggn <ggn.dbug@gmail.com>
2018-04-20 08:13:04 +0300

    Added Barbarian ST source code with the kind permission of their author.

---
+++ projects/barbarianst/rapu235.s
@@ -0,0 +1,181 @@
+;;
+;; U235SE Sample Bank
+;;
+;;
+;;
+
+			.dphrase
+
+RAPTOR_samplebank:
+
+sample0:	dc.l	sfx_epee						; start of sample - just the external label to U235 here
+			dc.l	sfx_epee_end					; end of sample - the external label to U235 with an added _end suffix
+			dc.l	0								; loop - start position
+			dc.l	0								; loop - end position
+			dc.w	0								; <NULL>
+			dc.b	0								; fine tune
+s0_vol:		dc.b	192								; volume
+			dc.l	8000							; default play rate
+
+sample1:	dc.l	sfx_touche1						; start of sample - just the external label to U235 here
+			dc.l	sfx_touche1_end					; end of sample - the external label to U235 with an added _end suffix
+			dc.l	0								; loop - start position
+			dc.l	0								; loop - end position
+			dc.w	0								; <NULL>
+			dc.b	0								; fine tune
+s1_vol:		dc.b	192								; volume
+			dc.l	8000							; default play rate
+
+sample2:	dc.l	sfx_tete						; start of sample - just the external label to U235 here
+			dc.l	sfx_tete_end					; end of sample - the external label to U235 with an added _end suffix
+			dc.l	0								; loop - start position
+			dc.l	0								; loop - end position
+			dc.w	0								; <NULL>
+			dc.b	0								; fine tune
+s2_vol:		dc.b	192								; volume
+			dc.l	8000							; default play rate
+
+sample3:	dc.l	sfx_mortdecap					; start of sample - just the external label to U235 here
+			dc.l	sfx_mortdecap_end				; end of sample - the external label to U235 with an added _end suffix
+			dc.l	0								; loop - start position
+			dc.l	0								; loop - end position
+			dc.w	0								; <NULL>
+			dc.b	0								; fine tune
+s3_vol:		dc.b	192								; volume
+			dc.l	8000							; default play rate
+
+sample4:	dc.l	sfx_cling1						; start of sample - just the external label to U235 here
+			dc.l	sfx_cling1_end					; end of sample - the external label to U235 with an added _end suffix
+			dc.l	0								; loop - start position
+			dc.l	0								; loop - end position
+			dc.w	0								; <NULL>
+			dc.b	0								; fine tune
+s4_vol:		dc.b	192								; volume
+			dc.l	8000							; default play rate
+
+sample5:	dc.l	sfx_pied						; start of sample - just the external label to U235 here
+			dc.l	sfx_pied_end					; end of sample - the external label to U235 with an added _end suffix
+			dc.l	0								; loop - start position
+			dc.l	0								; loop - end position
+			dc.w	0								; <NULL>
+			dc.b	0								; fine tune
+s5_vol:		dc.b	192								; volume
+			dc.l	8000							; default play rate
+
+sample6:	dc.l	sfx_mort						; start of sample - just the external label to U235 here
+			dc.l	sfx_mort_end					; end of sample - the external label to U235 with an added _end suffix
+			dc.l	0								; loop - start position
+			dc.l	0								; loop - end position
+			dc.w	0								; <NULL>
+			dc.b	0								; fine tune
+s6_vol:		dc.b	192								; volume
+			dc.l	8000							; default play rate
+
+sample7:	dc.l	sfx_gameover					; start of sample - just the external label to U235 here
+			dc.l	sfx_gameover_end				; end of sample - the external label to U235 with an added _end suffix
+			dc.l	0								; loop - start position
+			dc.l	0								; loop - end position
+			dc.w	0								; <NULL>
+			dc.b	0								; fine tune
+s7_vol:		dc.b	192								; volume
+			dc.l	8000							; default play rate
+
+sample8:	dc.l	sfx_theme						; start of sample - just the external label to U235 here
+			dc.l	sfx_theme_end					; end of sample - the external label to U235 with an added _end suffix
+			dc.l	0								; loop - start position
+			dc.l	0								; loop - end position
+			dc.w	0								; <NULL>
+			dc.b	0								; fine tune
+s8_vol:		dc.b	192								; volume
+			dc.l	8000							; default play rate
+
+sample9:	dc.l	sfx_decapite						; start of sample - just the external label to U235 here
+			dc.l	sfx_decapite_end					; end of sample - the external label to U235 with an added _end suffix
+			dc.l	0								; loop - start position
+			dc.l	0								; loop - end position
+			dc.w	0								; <NULL>
+			dc.b	0								; fine tune
+s9_vol:		dc.b	192								; volume
+			dc.l	8000							; default play rate			
+
+sample10:	dc.l	sfx_touche2						; start of sample - just the external label to U235 here
+			dc.l	sfx_touche2_end					; end of sample - the external label to U235 with an added _end suffix
+			dc.l	0								; loop - start position
+			dc.l	0								; loop - end position
+			dc.w	0								; <NULL>
+			dc.b	0								; fine tune
+s10_vol:	dc.b	192								; volume
+			dc.l	8000							; default play rate	
+
+sample11:	dc.l	sfx_touche3						; start of sample - just the external label to U235 here
+			dc.l	sfx_touche3_end					; end of sample - the external label to U235 with an added _end suffix
+			dc.l	0								; loop - start position
+			dc.l	0								; loop - end position
+			dc.w	0								; <NULL>
+			dc.b	0								; fine tune
+s11_vol:	dc.b	192								; volume
+			dc.l	8000							; default play rate	
+
+sample12:	dc.l	sfx_cling2						; start of sample - just the external label to U235 here
+			dc.l	sfx_cling2_end					; end of sample - the external label to U235 with an added _end suffix
+			dc.l	0								; loop - start position
+			dc.l	0								; loop - end position
+			dc.w	0								; <NULL>
+			dc.b	0								; fine tune
+s12_vol:	dc.b	192								; volume
+			dc.l	8000							; default play rate
+
+sample13:	dc.l	sfx_cling3						; start of sample - just the external label to U235 here
+			dc.l	sfx_cling3_end					; end of sample - the external label to U235 with an added _end suffix
+			dc.l	0								; loop - start position
+			dc.l	0								; loop - end position
+			dc.w	0								; <NULL>
+			dc.b	0								; fine tune
+s13_vol:	dc.b	192								; volume
+			dc.l	8000							; default play rate	
+
+sample14:	dc.l	sfx_cling4						; start of sample - just the external label to U235 here
+			dc.l	sfx_cling4_end					; end of sample - the external label to U235 with an added _end suffix
+			dc.l	0								; loop - start position
+			dc.l	0								; loop - end position
+			dc.w	0								; <NULL>
+			dc.b	0								; fine tune
+s14_vol:	dc.b	192								; volume
+			dc.l	8000							; default play rate
+
+sample15:	dc.l	sfx_coupdeboule					; start of sample - just the external label to U235 here
+			dc.l	sfx_coupdeboule_end				; end of sample - the external label to U235 with an added _end suffix
+			dc.l	0								; loop - start position
+			dc.l	0								; loop - end position
+			dc.w	0								; <NULL>
+			dc.b	0								; fine tune
+s15_vol:	dc.b	192								; volume
+			dc.l	8000							; default play rate
+
+sample16:	dc.l	sfx_tombe						; start of sample - just the external label to U235 here
+			dc.l	sfx_tombe_end					; end of sample - the external label to U235 with an added _end suffix
+			dc.l	0								; loop - start position
+			dc.l	0								; loop - end position
+			dc.w	0								; <NULL>
+			dc.b	0								; fine tune
+s16_vol:	dc.b	192								; volume
+			dc.l	8000							; default play rate
+
+sample17:	dc.l	sfx_protege						; start of sample - just the external label to U235 here
+			dc.l	sfx_protege_end					; end of sample - the external label to U235 with an added _end suffix
+			dc.l	0								; loop - start position
+			dc.l	0								; loop - end position
+			dc.w	0								; <NULL>
+			dc.b	0								; fine tune
+s17_vol:	dc.b	192								; volume
+			dc.l	8000							; default play rate
+
+sample18:	dc.l	sfx_roule						; start of sample - just the external label to U235 here
+			dc.l	sfx_roule_end					; end of sample - the external label to U235 with an added _end suffix
+			dc.l	0								; loop - start position
+			dc.l	0								; loop - end position
+			dc.w	0								; <NULL>
+			dc.b	0								; fine tune
+s18_vol:	dc.b	192								; volume
+			dc.l	8000							; default play rate			
+; END OF FILE.
